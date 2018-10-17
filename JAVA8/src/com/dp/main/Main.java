e00637ce2011d22d5e8e7a491a3ea0834f72eec3
anjali <anjali@DESKTOP-GPHGSVM>
2018-10-17 23:29:45 +0530

    stream example

--- JAVA8/src/com/dp/main/Main.java
+++ JAVA8/src/com/dp/main/Main.java
@@ -1,5 +1,9 @@
 package com.dp.main;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.dp.java.lambda.LambdaEx;
 
 /**
@@ -10,6 +14,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		LambdaEx.lmbda();
+		Stream<String> language =  Stream.of("tamil","telgu","hindi",null,"kannada");
+		List<String> list =	language.filter(item->item!=null).collect(Collectors.toList());
+		list.forEach(System.out::println);
 	}
 
 }
