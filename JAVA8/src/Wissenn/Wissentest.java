98fb6f7fad692dd3f7b8cd7f61fbc5f1af0b3091
dhiraj <dhiraj@DHIRAJ-PC>
2026-03-15 00:42:57 +0530

    wissentest

---
+++ JAVA8/src/Wissenn/Wissentest.java
@@ -0,0 +1,27 @@
+package Wissenn;
+
+public class Wissentest {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+
+    static public void putNewRating(String movie, double rating) {
+    	StringBuilder ingbuilder  = new StringBuilder(movie);
+    	
+    }
+
+    
+    public double getAverageRating(String movie) {
+		return 0;
+        // YOUR CODE HERE
+    }
+
+    public int getRatingCount(String movie) {
+		return 0;
+        // YOUR CODE HERE
+    }
+
+}
