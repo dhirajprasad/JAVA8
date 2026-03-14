71b8d74a8a7a6b214b60a82e094977925a5c8488
dhiraj <dhiraj@DHIRAJ-PC>
2026-03-15 00:47:46 +0530

    executorframework test

--- JAVA8/src/com/dp/thread/ExecutorFramework.java
+++ JAVA8/src/com/dp/thread/ExecutorFramework.java
@@ -1,39 +1,54 @@
 package com.dp.thread;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class ExecutorFramework {
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-
-		Thread WorkerA = new Thread(new Task());
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		@SuppressWarnings("rawtypes")
-		Future f = executorService.submit(WorkerA);
-		System.out.println("submitted the task");
+		
+		ExecutorService ex = Executors.newFixedThreadPool(2);
+		Future<String> worker1 =  ex.submit(()->{
+			System.out.println("worker1"+Thread.currentThread().getName());
+			
+			Thread.currentThread().sleep(6000);
+			return "finished-worker1";
+		});
+		Future<String> worker2 =  ex.submit(()->{
+			System.out.println("worker2"+Thread.currentThread().getName());
+			Thread.currentThread().sleep(6000);
+			return "finished-worker2";
+		});
+		System.out.println("=====");
 		try {
-			Object o= f.get();
+			String oo = null;
+			try {
+				oo = worker2.get(10, TimeUnit.SECONDS);
+			} catch (TimeoutException e) {
+				e.printStackTrace();
+			}
+			System.out.println(oo);
+			String o= worker1.get();
+			
 			System.out.println(o);
-		} catch (InterruptedException | ExecutionException e) {
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+		
 
 }
 
-class Task implements Runnable{
 
-	@Override
-	public void run() {
-		int i = 0;//Integer.MIN_VALUE/6000;
-		while (i<9){
-			i++;
-			System.out.println(i);
-		}
-		System.out.println(Thread.currentThread());
-	}
 	
 }
